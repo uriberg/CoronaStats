50e4d551d9f33150e2cd6f7e77d2a3e702934745
Uri_Berger <uribe1927@gmail.com>
2020-05-13 02:52:04 +0300

    finish cards calculations

--- src/containers/country.tsx
+++ src/containers/country.tsx
@@ -22,16 +22,26 @@ class Country extends Component<CountryProps> {
         totalCases: "",
         totalDeaths: "",
         totalRecovered: "",
-        currentlyInfected: "",
+        currentlyInfected: 0,
         deathsPerOneMillion: "",
         testsPerOneMillion: "",
+        criticalCondition: "",
+        activeCasesChange: "",
+        worseActiveCasesDate: "",
+        worseActiveCasesNumber: 0,
+        worseNewCasesDate: "",
+        worseNewCasesNumber: 0,
+        worseNewDeathsDate: "",
+        worseNewDeathsNumber: 0,
+        lastSevenDaysNewCases: 0,
+        lastSevenDaysNewDeaths: 0,
+        todayCases: 0,
+        todayDeaths: 0,
         showGraphs: false
     };
 
     componentDidMount(): void {
         console.log(this.props.match.params.country);
-
-
         axios.get("https://api.covid19api.com/total/dayone/country/" + this.props.match.params.country)
             .then(response => {
                 console.log(response.data);
@@ -39,6 +49,12 @@ class Country extends Component<CountryProps> {
                 let temp = [];
                 let firstDate = dateformat(data[0].Date, "mmm dS");
                 console.log(firstDate);
+                let worseActiveCasesNumber = data[0].Active;
+                let worseActiveCasesDate = firstDate;
+                let worseNewCasesNumber = data[0].Confirmed;
+                let worseNewCasesDate = firstDate;
+                let worseNewDeathsNumber = data[0].Deaths;
+                let worseNewDeathsDate = firstDate;
                 const firstDayObject = {
                     x: 0,
                     y: data[0].Confirmed,
@@ -47,6 +63,7 @@ class Country extends Component<CountryProps> {
                     Deaths: data[0].Deaths,
                     ActiveCases: data[0].Active
                 };
+                console.log(worseNewCasesNumber);
                 temp.push(firstDayObject);
                 for (let i = 1; i < data.length; i++) {
                     let daily = {
@@ -57,34 +74,131 @@ class Country extends Component<CountryProps> {
                         Deaths: data[i].Deaths - data[i - 1].Deaths,
                         ActiveCases: data[i].Active
                     };
+                    if (daily.ActiveCases > worseActiveCasesNumber) {
+                        worseActiveCasesNumber = daily.ActiveCases;
+                        worseActiveCasesDate = dateformat(data[i].Date, "mmm dS");
+                    }
+                    if (daily.Cases > worseNewCasesNumber){
+                        worseNewCasesNumber = daily.Cases;
+                        worseNewCasesDate = dateformat(data[i].Date, "mmm dS");
+                    }
+                    if (daily.Deaths > worseNewDeathsNumber) {
+                        worseNewDeathsNumber = daily.Deaths;
+                        worseNewDeathsDate = dateformat(data[i].Date, "mmm dS");
+                    }
+
+                    console.log(daily);
                     temp.push(daily);
                 }
+
+                let lastSevenDaysNewCases = 0;
+                let lastSevenDaysNewDeaths = 0;
+                for (let i = temp.length - 1, j = 0; j < 7; i--, j++){
+                    lastSevenDaysNewCases += temp[i].Cases;
+                    lastSevenDaysNewDeaths += temp[i].Deaths;
+                }
+                console.log((lastSevenDaysNewCases / 7).toFixed(2));
+                console.log((lastSevenDaysNewDeaths / 7).toFixed(2));
+                console.log(worseNewDeathsNumber);
+                console.log(worseNewCasesNumber);
                 console.log(temp);
-                this.setState({dailyCases: temp, firstCase: firstDayObject.Date});
+                this.setState({
+                    dailyCases: temp,
+                    firstCase: firstDayObject.Date,
+                    worseActiveCasesNumber: worseActiveCasesNumber,
+                    worseActiveCasesDate: worseActiveCasesDate,
+                    worseNewCasesNumber: worseNewCasesNumber,
+                    worseNewCasesDate: worseNewCasesDate,
+                    worseNewDeathsNumber: worseNewDeathsNumber,
+                    worseNewDeathsDate:  worseNewDeathsDate,
+                    lastSevenDaysNewCases: lastSevenDaysNewCases,
+                    lastSevenDaysNewDeaths: lastSevenDaysNewDeaths
+                });
             })
             .catch(err => {
                 console.log(err)
             });
+
         axios.get("https://disease.sh/v2/countries/" + this.props.match.params.country + "?yesterday=false")
             .then(response => {
+                console.log(response.data);
                 this.setState({
                     totalCases: response.data.cases,
                     totalDeaths: response.data.deaths,
                     totalRecovered: response.data.recovered,
                     currentlyInfected: response.data.active,
                     deathsPerOneMillion: response.data.deathsPerOneMillion,
-                    testsPerOneMillion: response.data.testsPerOneMillion
+                    testsPerOneMillion: response.data.testsPerOneMillion,
+                    criticalCondition: response.data.critical,
+                    todayCases: response.data.todayCases,
+                    todayDeaths: response.data.todayDeaths
                 });
+                this.getYesterdayStats();
             })
             .catch(err => {
                 console.log(err)
             });
     }
 
-    componentDidUpdate(prevProps: Readonly<CountryProps>, prevState: Readonly<{}>, snapshot?: any): void {
-        console.log(this.state.showGraphs);
+    getYesterdayStats = () => {
+        axios.get("https://disease.sh/v2/countries/" + this.props.match.params.country + "?yesterday=true")
+            .then(response => {
+                console.log(response.data);
+                console.log('before for loop');
+                console.log(this.state.currentlyInfected);
+                let activeCasesChange = +(this.state.currentlyInfected / response.data.active * 100 - 100).toFixed(2);
+                console.log(activeCasesChange);
+                    this.setState({activeCasesChange: activeCasesChange});
+                    console.log(this.state);
+                    if (this.state.currentlyInfected > this.state.worseActiveCasesNumber || response.data.active > this.state.worseActiveCasesNumber){
+                    if (activeCasesChange > 0){
+                        this.setState({
+                            worseActiveCasesDate: "Today",
+                            worseActiveCasesNumber: this.state.currentlyInfected
+                        });
+                    }
+                    else {
+                        this.setState({
+                            worseActiveCasesDate: "Yesterday",
+                            worseActiveCasesNumber: response.data.active
+                        });
+                    }
+                }
 
-    }
+                if (this.state.todayCases > this.state.worseNewCasesNumber || response.data.todayCases > this.state.worseNewCasesNumber){
+                    if (this.state.todayCases > response.data.todayCases){
+                        this.setState({
+                            worseNewCasesDate: "Today",
+                            worseNewCasesNumber: this.state.todayCases
+                        });
+                    }
+                    else {
+                        this.setState({
+                            worseNewCasesDate: "Yesterday",
+                            worseNewCasesNumber: response.data.todayCases
+                        });
+                    }
+                }
+
+                if (this.state.todayDeaths > this.state.worseNewDeathsNumber || response.data.todayDeaths > this.state.worseNewDeathsNumber){
+                    if (activeCasesChange > 0){
+                        this.setState({
+                            worseNewDeathsDate: "Today",
+                            worseNewDeathsNumber: this.state.todayDeaths
+                        });
+                    }
+                    else {
+                        this.setState({
+                            worseNewDeathsDate: "Yesterday",
+                            worseNewDeathsNumber: response.data.todayDeaths
+                        });
+                    }
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    };
 
     showGraphs = () => {
         //this.setState({showGraphs: true});
@@ -117,29 +231,29 @@ class Country extends Component<CountryProps> {
                     <div className="card card--new-cases">
                         <h3 className="card__title">New Cases</h3>
                         <ul className="card__list">
-                            <li className="card__item">Worse Day: <span>{this.state.firstCase}</span></li>
-                            <li className="card__item">Today: <span>{this.state.totalCases}</span></li>
-                            <li className="card__item">Last 7 days: <span>{this.state.totalDeaths}</span></li>
-                            <li className="card__item">Last 7 days Average: <span>{this.state.totalRecovered}</span>
+                            <li className="card__item">Worse Day: <span>{this.state.worseNewCasesDate}</span></li>
+                            <li className="card__item">Today: <span>{this.state.todayCases}</span></li>
+                            <li className="card__item">Last 7 days: <span>{this.state.lastSevenDaysNewCases}</span></li>
+                            <li className="card__item">Last 7 days Average: <span>{(this.state.lastSevenDaysNewCases / 7).toFixed(2)}</span>
                             </li>
                         </ul>
                     </div>
                     <div className="card card--active-cases">
                         <h3 className="card__title">Active Cases</h3>
                         <ul className="card__list">
-                            <li className="card__item">Worse Day: <span>{this.state.firstCase}</span></li>
-                            <li className="card__item">Currently Infected: <span>{this.state.totalCases}</span></li>
-                            <li className="card__item">Change from yesterday: <span>{this.state.totalDeaths}</span></li>
-                            <li className="card__item">Critical Condition: <span>{this.state.totalRecovered}</span></li>
+                            <li className="card__item">Worse Day: <span>{this.state.worseActiveCasesDate}</span></li>
+                            <li className="card__item">Currently Infected: <span>{this.state.currentlyInfected}</span></li>
+                            <li className="card__item">Change from yesterday: <span>{this.state.activeCasesChange}%</span></li>
+                            <li className="card__item">Critical Condition: <span>{this.state.criticalCondition}</span></li>
                         </ul>
                     </div>
                     <div className="card card--deaths">
                         <h3 className="card__title">Deaths</h3>
                         <ul className="card__list">
-                            <li className="card__item">Worse Day: <span>{this.state.firstCase}</span></li>
-                            <li className="card__item">Today: <span>{this.state.totalCases}</span></li>
-                            <li className="card__item">Last 7 days: <span>{this.state.totalDeaths}</span></li>
-                            <li className="card__item">Last 7 days average: <span>{this.state.totalRecovered}</span>
+                            <li className="card__item">Worse Day: <span>{this.state.worseNewDeathsDate}</span></li>
+                            <li className="card__item">Today: <span>{this.state.todayDeaths}</span></li>
+                            <li className="card__item">Last 7 days: <span>{this.state.lastSevenDaysNewDeaths}</span></li>
+                            <li className="card__item">Last 7 days average: <span>{(this.state.lastSevenDaysNewDeaths / 7).toFixed(2)}</span>
                             </li>
                         </ul>
                     </div>
