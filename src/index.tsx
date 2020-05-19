cfd0d6975d2f1dfaf6f63f0bdd1011701e0a04e0
Uri_Berger <uribe1927@gmail.com>
2020-05-20 00:29:31 +0300

    editing the manifest json file

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
-import CoronaTable from "./components/CoronaTable";
 import Continent from "./containers/continent";
 import Country from "./containers/country";
 import Header from "./layout/header";
-import "./index.scss";
 import "./sass/main.scss";
 
 const routing = (
     <Router>
-        <div className="websiteWrapper">
             <Header/>
-            <div className="containersWrapper">
                 <Switch>
                     <Route exact path="/" component={Continent}/>
                     <Route path="/continent/:continent" component={Continent}/>
                     <Route path="/country/:country" component={props => (<Country {...props} />)}/>
-                    <Route component={CoronaTable}/>
                 </Switch>
-            </div>
-        </div>
     </Router>
 );
 
