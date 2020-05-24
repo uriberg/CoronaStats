8f8f4a35b7a804ce9ecc80c00b93724e5976b454
uriberg <uribe1927@gmail.com>
2020-05-24 19:31:32 +0300

    register the service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,7 +21,9 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
+  console.log('register function');
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    console.log('in production mode');
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
